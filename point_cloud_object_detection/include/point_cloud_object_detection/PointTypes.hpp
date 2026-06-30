0488d095dc0a9c08c4ae3e041cf4415daa22c2a9
Bastian Lampe <bastian.lampe@ika.rwth-aachen.de>
2026-06-30 12:49:12 +0000

    clean up

--- point_cloud_object_detection/include/point_cloud_object_detection/PointTypes.hpp
+++ point_cloud_object_detection/include/point_cloud_object_detection/PointTypes.hpp
@@ -5,22 +5,10 @@
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <Eigen/Core>
 
 namespace point_cloud_object_detection {
-
-struct PointXYZRV {
-  PCL_ADD_POINT4D;
-  float reflectivity = 0.0f;
-  float velocity = 0.0f;
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-} EIGEN_ALIGN16;
 
 using Point = pcl::PointXYZI;
 using PointCloud = pcl::PointCloud<Point>;
 
 }  // namespace point_cloud_object_detection
-
-POINT_CLOUD_REGISTER_POINT_STRUCT(point_cloud_object_detection::PointXYZRV,
-                                  (float, x, x)(float, y, y)(float, z, z)(float, reflectivity,
-                                                                          reflectivity)(float, velocity, velocity))
