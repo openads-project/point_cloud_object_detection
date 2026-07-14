94ac9904c460436ce6add5832e6bddb68a0d81de
Bastian Lampe <bastian.lampe@ika.rwth-aachen.de>
2026-07-14 11:11:55 +0200

    handle rclpy shutdown more gracefully in main function

--- demo/rviz/reset_rviz_on_clock_jump.py
+++ demo/rviz/reset_rviz_on_clock_jump.py
@@ -80,11 +80,12 @@ def main():
     node = RvizClockResetWatchdog(rviz_command)
     try:
         rclpy.spin(node)
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
         pass
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 
 if __name__ == "__main__":
